a0ca0211b97152b8594cb207c45ca8c92ac71cbb
Tao <werewaswas@live.com>
2018-08-03 07:24:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 
 function exactMatch(drivers, attribute) {
   return drivers.filter(function (driver) {
-    key = attribute.keys()[0]
+    let key = attribute.keys()[0]
     return driver[key] == attribute[key]
   })
 }
